5a0b524b8fe3dfe117c8875e8c80b74e2ca2594b
shreyatyagi788 <shreyatyagi788@gmail.com>
2024-01-02 16:43:49 -0500

    code modified

--- src/test/java/day2/waysToCreatePostRequestBody.java
+++ src/test/java/day2/waysToCreatePostRequestBody.java
@@ -19,32 +19,32 @@ import java.util.HashMap;
 public class waysToCreatePostRequestBody {
 	int id;
 	
-	//post request body using hashmap.
-//	@Test(priority = 1)
-//	void testPostUsingHashMap() {
-//		HashMap data = new HashMap();
-//		data.put("name", "mini");
-//		data.put("country", "usa");
-//		
-//		String courseArr[] = {"C","C++"}; 
-//		data.put("courses", courseArr);
-//		
-//		id = given()
-//			.contentType("application/json")
-//			.body(data)
-//		.when()
-//			.post("http://localhost:3000/students")
-//			.jsonPath().getInt("id");
-////		.then()
-////			.statusCode(201)
-////			.body("name",equalTo("mini"))
-////			.body("country",equalTo("usa"))
-////			.body("courses[0]", equalTo("C"))
-////			.body("courses[1]",equalTo("C++"))
-////			.header("Content-Type","application/json; charset=utf-8")
-////			.log().all();
-//		
-//	}
+	//1.post request body using hashmap.
+	//@Test(priority = 1)
+	void testPostUsingHashMap() {
+		HashMap data = new HashMap();
+		data.put("name", "mini");
+		data.put("country", "usa");
+		
+		String courseArr[] = {"C","C++"}; 
+		data.put("courses", courseArr);
+		
+		id = given()
+			.contentType("application/json")
+			.body(data)
+		.when()
+			.post("http://localhost:3000/students")
+			.jsonPath().getInt("id");
+//		.then()
+//			.statusCode(201)
+//			.body("name",equalTo("mini"))
+//			.body("country",equalTo("usa"))
+//			.body("courses[0]", equalTo("C"))
+//			.body("courses[1]",equalTo("C++"))
+//			.header("Content-Type","application/json; charset=utf-8")
+//			.log().all();
+		
+	}
 	@Test(priority=2)
 	void testDelete() {
 		given()
@@ -54,57 +54,57 @@ public class waysToCreatePostRequestBody {
 			.statusCode(200)
 			.log().all();
 	}
-//	
-//	//post request body using org.json library.
-//		@Test(priority = 1)
-//		void testPostUsingOrgJsonLibrary() {
-//			JSONObject data = new JSONObject();
-//			data.put("name","ayu");
-//			data.put("country", "India");
-//			String coursesArr[] = {"python","ruby"};
-//			data.put("courses", coursesArr);
-//			
-//			id = given()
-//				.contentType("application/json")
-//				.body(data.toString())
-//			.when()
-//				.post("http://localhost:3000/students")
-//				.jsonPath().getInt("id");
-////			.then()
-////				.statusCode(201)
-////				.body("name",equalTo("mini"))
-////				.body("country",equalTo("usa"))
-////				.body("courses[0]", equalTo("C"))
-////				.body("courses[1]",equalTo("C++"))
-////				.header("Content-Type","application/json; charset=utf-8")
-////				.log().all();
-//			
-//		}
-//	//post request body using POJO.
-//		@Test(priority = 1)
-//		void testPostUsingPOJO() {
-//			pojo_postRequest data = new pojo_postRequest();
-//			data.setName("nishkarsh");
-//			data.setCountry("india");
-//			String courseArr[]= {"mysql","python"};
-//			data.setCourses(courseArr);
-//		    given()
-//				.contentType("application/json")
-//				.body(data)
-//			.when()
-//				.post("http://localhost:3000/students")
-//			
+	
+	//2.post request body using org.json library.
+		//@Test(priority = 1)
+		void testPostUsingOrgJsonLibrary() {
+			JSONObject data = new JSONObject();
+			data.put("name","ayu");
+			data.put("country", "India");
+			String coursesArr[] = {"python","ruby"};
+			data.put("courses", coursesArr);
+			
+			id = given()
+				.contentType("application/json")
+				.body(data.toString())
+			.when()
+				.post("http://localhost:3000/students")
+				.jsonPath().getInt("id");
 //			.then()
 //				.statusCode(201)
-//				.body("name",equalTo("nishkarsh"))
-//				.body("country",equalTo("india"))
-//				.body("courses[0]", equalTo("mysql"))
-//				.body("courses[1]",equalTo("python"))
+//				.body("name",equalTo("mini"))
+//				.body("country",equalTo("usa"))
+//				.body("courses[0]", equalTo("C"))
+//				.body("courses[1]",equalTo("C++"))
 //				.header("Content-Type","application/json; charset=utf-8")
 //				.log().all();
-//		
-//	}
-//
+			
+		}
+	//post request body using POJO.
+		//@Test(priority = 1)
+		void testPostUsingPOJO() {
+			pojo_postRequest data = new pojo_postRequest();
+			data.setName("nishkarsh");
+			data.setCountry("india");
+			String courseArr[]= {"mysql","python"};
+			data.setCourses(courseArr);
+		    given()
+				.contentType("application/json")
+				.body(data)
+			.when()
+				.post("http://localhost:3000/students")
+			
+			.then()
+				.statusCode(201)
+				.body("name",equalTo("nishkarsh"))
+				.body("country",equalTo("india"))
+				.body("courses[0]", equalTo("mysql"))
+				.body("courses[1]",equalTo("python"))
+				.header("Content-Type","application/json; charset=utf-8")
+				.log().all();
+		
+	}
+
 	//post request body using external json file.
 			@Test(priority = 1)
 			void testPostUsingexternalJsonFile() throws FileNotFoundException {
@@ -113,7 +113,7 @@ public class waysToCreatePostRequestBody {
 				JSONTokener jt = new JSONTokener(fr);
 				JSONObject data = new JSONObject(jt);
 			    given()
-					.contentType(ContentType.JSON)
+					.contentType("application/json")
 					.body(data.toString())
 				.when()
 					.post("http://localhost:3000/students")

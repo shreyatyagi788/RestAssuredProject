b682edb0178e918c63a3656360556f40fbd31426
shreyatyagi788 <shreyatyagi788@gmail.com>
2024-01-03 15:41:20 -0500

    added another approact of parsing jsonResponse.

--- src/test/java/day5/parsingJsonResponse.java
+++ src/test/java/day5/parsingJsonResponse.java
@@ -13,30 +13,56 @@ import org.testng.annotations.Test;
 import io.restassured.response.Response;
 
 public class parsingJsonResponse {
-	@Test
-	void testJsonResponse() {
+	//Approach 1
+	@Test(priority=1)
+	void testSmallJsonResponse() {
+		given()
+			.contentType("application/json")
+		.when()
+			.get("http://localhost:3000/colourbook")
+		.then()
+			.statusCode(200)
+			.header("Content-Type","application/json; charset=utf-8")
+			.log().all()
+			.body("[3].bookname",equalTo("rrr"));
+
+		
+	}
+	@Test(priority=2)
+	void testLargeJsonResponse() {
+		
+		//Approach 2
 		
 		Response res = given()
 		
 		.when()
 			.get("http://localhost:3000/colourbook");
 		
+		Assert.assertEquals(res.getStatusCode(),200); //validation 1
+		
+		String bookName = res.jsonPath().get("[3].bookname").toString();
+		
+		Assert.assertEquals(bookName, "rrr"); //validation 2
+		
 		JSONArray jp = new JSONArray(res.asString());
+		int size = jp.length();
+		System.out.println("length of array is :"+size);
 		//int s = jp.getInt("data.size()");
 		//System.out.println("length of array is :"+s);
 		
 //		List<String> data = jp.getList("bookname");
-		String data = jp.getJSONObject(0).getString("bookname");
+//		String data = jp.getJSONObject(0).getString("bookname");
 //		System.out.println("data is :"+data);
-//		boolean status = false;
-//		for(String item:data) {
-//			if(item.equals("xyz")) {
-//				status = true;
-//				break;
-//			}
-//			
-//		}
-		Assert.assertEquals(data, "xyz");
+		boolean status = false;
+		for(int i=0;i<size;i++) {
+			String item = jp.getJSONObject(i).get("bookname").toString();
+			if(item.equals("xyz")) {
+				status = true;
+				break;
+			}
+			
+		}
+//		Assert.assertEquals(data, "xyz");
 		}
 //		.then()
 //			.statusCode(200);
